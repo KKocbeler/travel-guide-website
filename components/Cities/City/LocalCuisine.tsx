3a789aa7c3bcd82ec70b7b95ad60f5f20e1d8642
Kemal Koçbeler <kkocbeler@gmail.com>
2024-09-01 22:10:19 +0300

    Local cuisine

--- components/Cities/City/LocalCuisine.tsx
+++ components/Cities/City/LocalCuisine.tsx
@@ -7,33 +7,48 @@ const LocalCuisine = () => {
             <div className="header-title">Local Cuisine</div>
             <div className="food-list">
                 <div className="food">
-                    <div className="title">Croissant</div>
                     <div className="image">
                         <img src="https://assets.zuckerjagdwurst.com/u4q97pkqszcdxj62j8adg1z9xdpz/1200/640/75/true/center/undefined.jpg?animated=undefined" alt="Croissant" />
                     </div>
-                </div>
-                <div className="food">
-                    <div className="title">Croissant</div>
-                    <div className="image">
-                        <img src="https://assets.zuckerjagdwurst.com/u4q97pkqszcdxj62j8adg1z9xdpz/1200/640/75/true/center/undefined.jpg?animated=undefined" alt="Croissant" />
+                    <div className="body">
+                        <div className="title">Croissant</div>
+                        <div className="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, dolor.</div>
                     </div>
                 </div>
                 <div className="food">
-                    <div className="title">Croissant</div>
                     <div className="image">
                         <img src="https://assets.zuckerjagdwurst.com/u4q97pkqszcdxj62j8adg1z9xdpz/1200/640/75/true/center/undefined.jpg?animated=undefined" alt="Croissant" />
                     </div>
-                </div>
-                <div className="food">
-                    <div className="title">Croissant</div>
-                    <div className="image">
-                        <img src="https://assets.zuckerjagdwurst.com/u4q97pkqszcdxj62j8adg1z9xdpz/1200/640/75/true/center/undefined.jpg?animated=undefined" alt="Croissant" />
+                    <div className="body">
+                        <div className="title">Croissant</div>
+                        <div className="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, dolor.</div>
                     </div>
                 </div>
                 <div className="food">
-                    <div className="title">Croissant</div>
                     <div className="image">
                         <img src="https://assets.zuckerjagdwurst.com/u4q97pkqszcdxj62j8adg1z9xdpz/1200/640/75/true/center/undefined.jpg?animated=undefined" alt="Croissant" />
+                    </div>
+                    <div className="body">
+                        <div className="title">Croissant</div>
+                        <div className="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, dolor.</div>
+                    </div>
+                </div>
+                <div className="food">
+                    <div className="image">
+                        <img src="https://assets.zuckerjagdwurst.com/u4q97pkqszcdxj62j8adg1z9xdpz/1200/640/75/true/center/undefined.jpg?animated=undefined" alt="Croissant" />
+                    </div>
+                    <div className="body">
+                        <div className="title">Croissant</div>
+                        <div className="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, dolor.</div>
+                    </div>
+                </div>
+                <div className="food">
+                    <div className="image">
+                        <img src="https://assets.zuckerjagdwurst.com/u4q97pkqszcdxj62j8adg1z9xdpz/1200/640/75/true/center/undefined.jpg?animated=undefined" alt="Croissant" />
+                    </div>
+                    <div className="body">
+                        <div className="title">Croissant</div>
+                        <div className="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, dolor.</div>
                     </div>
                 </div>
             </div>
