0d18164ca744182e294ff189d7d3c92844a2527e
Kemal Koçbeler <kkocbeler@gmail.com>
2024-09-09 00:18:08 +0300

    editing css

--- components/Cities/City/InterestingInfo.tsx
+++ components/Cities/City/InterestingInfo.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import '@/components/Cities/City/InterestingInfo.css';
 import { City } from '@/types/CityTypes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 
 interface InterestingProps {
     city: City
@@ -14,11 +16,12 @@ const InterestingInfo:React.FC<InterestingProps> = ({city}) => {
             <div className="header-title">Interesting Information and Facts</div>
             <div className="facts">
                 {
-                    interestingTips ?.map((info, index: number) => (
+                    interestingTips?.map((info, index: number) => (
                         <div className="fact" key={index}>
                             <div className="fact-card">
                                 <div className="title">{info.name}</div>
                                 <p className='description'>{info.info}</p>
+                                <FontAwesomeIcon icon={faBookmark} />
                             </div>
                         </div>
                     ))
