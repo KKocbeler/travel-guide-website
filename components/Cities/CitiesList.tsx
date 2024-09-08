40415d16575dbb24ccaf21638992f0c57a7da59b
Kemal Koçbeler <kkocbeler@gmail.com>
2024-09-08 19:30:23 +0300

    Cities Page

--- components/Cities/CitiesList.tsx
+++ components/Cities/CitiesList.tsx
@@ -1,36 +1,45 @@
-"use client"
+"use client";
 import '@/components/Cities/CitiesList.css';
 import { CityContext } from '@/context/CityContext';
 import { City } from '@/types/CityTypes';
 import Link from 'next/link';
 import { useContext } from 'react';
+import Loading from '../Pieces/Loading';
 
 const CitiesList = () => {
-    const {cities, loading, error} = useContext(CityContext);
-  return (
-    <div className='cities'>
-        <div className="title">Cities</div>
-        <div className="city-list">
-            {
-                cities?.map((city:City, index:string) => (
-                    <div className="city-card" key={index}>
-                        <div className="city-card-body">
-                            <div className="card-img">
-                                <Link href={`/cities/${city.name}`}>
-                                    <img src={city.img} alt={city.name} />
-                                </Link>
-                            </div>
-                        </div>
-                        <div className="city-name">
-                            <Link href={`/cities/${city.name}`}>{city.name}</Link>
-                        </div>
-                    </div>
-                ))
-            }
+    const { cities, loading, error } = useContext(CityContext);
 
+    return (
+        <div className='cities'>
+            <div className="title">Cities</div>
+            {
+                loading ? (
+                    <Loading />
+                ) : error ? (
+                    <p>Hata</p>
+                ) : (
+                    <div className="city-list">
+                        {
+                            cities?.map((city: City, index: number) => (
+                                <div className="city-card" key={index}>
+                                    <div className="city-card-body">
+                                        <div className="card-img">
+                                            <Link href={`/cities/${city.name}`}>
+                                                <img src={city.img} alt={city.name} />
+                                            </Link>
+                                        </div>
+                                    </div>
+                                    <div className="city-name">
+                                        <Link href={`/cities/${city.name}`}>{city.name}</Link>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                )
+            }
         </div>
-    </div>
-  )
+    );
 }
 
-export default CitiesList
+export default CitiesList;
